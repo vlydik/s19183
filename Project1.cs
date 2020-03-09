5704734a76677e038bd6b8afeeee7eb9bb630d27
Vladyslav <vlad.K2001@gmail.com>
2020-03-09 22:48:15 +0100

    123

---
+++ Project1.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace DefaultNamespace
+{
+    class Project1
+    {
+        static async Task Main(string[] args)
+        {
+            var url = "www.pja.edu.pl";
+            
+        }
+    }
+}
