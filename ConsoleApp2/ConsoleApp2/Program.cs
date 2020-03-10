704697d7359dce877a4baca16a494edb1a5e5fef
Vladyslav <vlad.K2001@gmail.com>
2020-03-10 17:21:00 +0100

    123

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace ConsoleApp2
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string URL = @"https://www.pja.edu.pl";
+
+            using var httpclient = new HttpClient();
+            try
+            {
+                var response = await httpclient.GetAsync(URL);
+                if (response.IsSuccessStatusCode)
+                {
+                    var regex = new Regex("[a-z]+[a-z0-9]*@[a-z]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                    var responseContent = await response.Content.ReadAsStringAsync();
+
+                    var matches = regex.Matches(responseContent);
+
+
+                    if (!matches.Equals(null))
+                    {
+                        foreach (var match in matches)
+                        {
+                            Console.WriteLine("Email found: " + match);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No email addresses found.");
+                    }
+                }
+            }
+            catch(ArgumentException)
+            {
+                Console.WriteLine("Error while downloading the page.");
+            }
         }
+
     }
 }
