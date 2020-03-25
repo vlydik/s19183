44a9f77bda09d91a53931d85c9a151a1d03d6fba
Vladyslav <vlad.K2001@gmail.com>
2020-03-25 22:08:35 +0200

    Task#2

---
+++ TutorialSolution2/TutorialSolution2/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq.Expressions;
+using System.Net;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace TutorialSolution2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> list = new List<Student>();
+            string line = null;
+
+            Console.WriteLine("Enter a path to CSV file:");
+            String FilePathToCSV = Console.ReadLine();
+
+            Console.WriteLine("Enter a destination path:");
+            String DestinationPath = Console.ReadLine() + "result";
+            Console.WriteLine(DestinationPath);
+
+            Console.WriteLine("Enter data format:");
+            String DataFormat = Console.ReadLine();
+            DestinationPath = DestinationPath + "." + DataFormat;
+            Console.WriteLine(DestinationPath);
+
+            String LogFilePath = "/Users/vladyslav/RiderProjects/s19183/TutorialSolution2/TutorialSolution2/log.txt";
+            using (FileStream logging = new FileStream(LogFilePath,FileMode.Create))
+            {
+                try
+                {
+                    if (!File.Exists(FilePathToCSV))
+                    {
+                        AddText(logging, "Path to File CSV not found.");
+                        throw new FileNotFoundException("CSV file does not exist.");
+                    }
+
+                    if (File.Exists(DestinationPath))
+                    {
+                        File.Delete(DestinationPath);
+                    }
+                    
+
+                    using (FileStream fs = File.Create(DestinationPath)) 
+                    using(var stream = new StreamReader(File.OpenRead(FilePathToCSV)))
+                    {
+                        while ((line = stream.ReadLine()) != null)
+                        {
+                            string[] students = line.Split(',');
+
+                            if (students.Length < 9)
+                            {
+                                continue;
+                            }
+                            var student = new Student()
+                            {
+                                FirstName = students[0],
+                                LastName = students[1],
+                                IndexNumber = students[4],
+                                BirthDate = students[5],
+                                Email = students[6],
+                                MotherName = students[7],
+                                FatherName = students[8],
+                                studies = new Studies(){
+                                    Course = students[2],
+                                    StudiesMode = students[3]
+                            }
+                            }; 
+                            list.Add(student);
+                        }    
+                        XmlSerializer sr = new XmlSerializer(typeof(List<Student>), new XmlRootAttribute("university"));
+                        
+                        sr.Serialize(fs,list);
+                    }
+                }
+                catch (Exception Ex)
+                {
+                    Console.WriteLine(Ex.ToString());
+                    AddText(logging,"Exception caught.");
+                }
+            }
+        }
+
+        private static void AddText(FileStream fs, string value)
+        {
+            byte[] data = new UTF8Encoding(true).GetBytes(value);
+            fs.Write(data,0,data.Length);
+        }
+    }
+}
