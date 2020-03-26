1d90f1afd8440dbee616e64b8c0bfbff8b7f6e6e
Vladyslav <vlad.K2001@gmail.com>
2020-03-26 16:48:57 +0200

    A little bit of changes

--- Task3/Task3/Startup.cs
+++ Task3/Task3/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Task3.DAL;
 
 namespace Task3
 {
@@ -26,6 +27,7 @@ namespace Task3
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSingleton<IDbService, MockDbService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
