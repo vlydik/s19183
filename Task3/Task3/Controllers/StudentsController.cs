d1e00c8146a7e1d5f85e7760909a00b2830c3f4c
Vladyslav <vlad.K2001@gmail.com>
2020-03-26 14:02:02 +0200

    A little bit of changes

--- Task3/Task3/Controllers/StudentsController.cs
+++ Task3/Task3/Controllers/StudentsController.cs
@@ -1,5 +1,6 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
+using Task3.DAL;
 using Task3.Models;
 
 namespace Task3.Controllers
@@ -8,33 +9,52 @@ namespace Task3.Controllers
     [Route("api/students")]
     public class StudentsController : ControllerBase
     {
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudent(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
 
         [HttpGet("{id}")]
         public IActionResult GetStudent(int id)
         {
-            if (id == 1)
+            if(id == 1)
             {
                 return Ok("Kowalski");
-            }
-            else if (id == 2)
+            }else if(id == 2)
             {
                 return Ok("Malewski");
             }
 
             return NotFound("Cannot find the student");
+
         }
-
-        /*[HttpGet]
-        public string GetStudents(string orderBy)
-        {
-            return $"Kowalski, Malewski, Andrzejewski orderBy={orderBy}";
-        }*/
-
+        
         [HttpPost]
         public IActionResult CreateStudent(Student student)
         {
-            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            student.IndexNumber = $"s{new Random().Next(1, 2000)}";
             return Ok(student);
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteMethod(int id)
+        {
+            return Ok("deleted");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult PutMethod(int id)
+        {
+            return Ok("updated");
         }
     }
 }
---
+++ Task3/Task3/DAL/IDbService.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using Task3.Models;
+
+namespace Task3.DAL
+{
+    public interface IDbService
+    {
+        public IEnumerable<Student> GetStudents();
+    }
+}
